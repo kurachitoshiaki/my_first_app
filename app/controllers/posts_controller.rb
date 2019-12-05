8e9a671047572796865bc6570e0ffb549d701866
kurachitoshiaki <aru0220kuria0214@gmail.com>
2019-12-05 16:21:47 +0900

    Update posts_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,10 @@
 class PostsController < ApplicationController
   def index
-    @post = "インスタンス変数"
+    @posts = Post.all
+  end
+  def new
+  end
+  def create
+    Post.create(content: params[:content])
   end
 end
