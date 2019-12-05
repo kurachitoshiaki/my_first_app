665f7ddf5fc0a8ae4c2a7524c72d6cefc2ad82aa
kurachitoshiaki <aru0220kuria0214@gmail.com>
2019-12-05 16:30:15 +0900

    Update posts_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,9 +2,16 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+  
   def new
   end
+
   def create
-    Post.create(content: params[:content])
+    Post.create(post_params)
+  end
+
+  private
+  def post_params
+    params.permit(:content)
   end
 end
